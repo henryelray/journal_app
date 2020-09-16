7e965673f61103abfdc7d7f35948a69ec8d1244b
Henry Solomon <sehenrysolomon@outlook.com>
2020-09-16 06:10:42 -0700

    url path to display all topics

--- logs/urls.py
+++ logs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'logs'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
 ]
