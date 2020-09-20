0ea42f7887232974b6adfd735789d26e844fa422
Henry Solomon <sehenrysolomon@outlook.com>
2020-09-20 12:48:20 -0700

    function to display a specific topic

--- logs/models.py
+++ logs/models.py
@@ -11,3 +11,19 @@ class Topic(models.Model):
     def __str__(self):
 
         return self.text
+
+
+class Entry(models.Model):
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+
+        return f"{self.text[:50]}..."
+
+
--- logs/views.py
+++ logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Topic, Entry
 # Create your views here.
 
 
@@ -7,3 +7,25 @@ def index(request):
     """Home page of web app"""
 
     return render(request, 'logs/index.html')
+
+
+def topics(request):
+    """Displays all existing topics"""
+
+    topics = Topic.objects.order_by('-date_added')
+
+    context = {'topics':topics}
+
+    return render(request, 'logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Display a specific topic"""
+
+    topic = Topic.objects.get(id=topic_id)
+
+    entries = topic.entry_set.order_by('-date_added')
+
+    context = {'topic':topic,'topic_id':topic_id, 'entries':entries}
+
+    return render(request, 'logs/topic.html', context)
