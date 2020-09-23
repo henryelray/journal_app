6df95da95ae246cd060f05e93b1b32c839de9846
Henry Solomon <sehenrysolomon@outlook.com>
2020-09-23 07:55:00 -0700

    Final version of logs apps

--- logs/views.py
+++ logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
-
+from django.http import Http404
 # Create your views here.
 
 
@@ -16,7 +16,7 @@ def index(request):
 def topics(request):
     """Displays all existing topics"""
 
-    topics = Topic.objects.order_by('-date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('-date_added')
 
     context = {'topics':topics}
 
@@ -28,7 +28,8 @@ def topic(request, topic_id):
     """Display a specific topic with its associated entries"""
 
     topic = Topic.objects.get(id=topic_id)
-
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
 
     context = {'topic':topic,'topic_id':topic_id, 'entries':entries}
@@ -47,7 +48,9 @@ def new_topic(request):
         form = TopicForm(data=request.POST)
 
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('logs:topics')
 
     context = {'form':form}
@@ -65,12 +68,12 @@ def new_entry(request, topic_id):
         form = EntryForm()
 
     else:
-
         form = EntryForm(data=request.POST)
         if form.is_valid():
 
             new_entry = form.save(commit=False)
             new_entry.topic = topic
+
             new_entry.save()
             return redirect('logs:topic', topic_id=topic_id)
 
@@ -85,14 +88,20 @@ def edit_entry(request, entry_id):
 
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
+
     if request.method != 'POST':
         form = EntryForm(instance=entry)
 
     else:
         form = EntryForm(instance=entry, data=request.POST)
         entry = form.save(commit=False)
+
         entry.save()
         return redirect('logs:topic', topic.id)
+
     context = {'entry':entry,'topic':topic,'form':form}
 
     return render(request, 'logs/edit_entry.html', context)
