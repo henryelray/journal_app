86b03317669daa56eade7863917ddc9b5ca6ae16
Henry Solomon <sehenrysolomon@outlook.com>
2020-10-01 06:03:59 -0700

    changes made to setting.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'learning_log.urls'
@@ -132,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = 'users:login'
 
 # heroku settings
