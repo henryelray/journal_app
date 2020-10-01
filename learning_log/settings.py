8c50fbf7b213e7e218694bc3b872b56c63326552
Henry Solomon <sehenrysolomon@outlook.com>
2020-10-01 10:58:58 -0700

    debug variable changed

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -35,7 +35,7 @@ import environ
 SECRET_KEY = os.environ.get('SECRET_KEY','%mh&wvowp3)(jqhstz0e5iv7a@_v9)xttag7z)n015h9n7^bt0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
