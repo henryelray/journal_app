76d3ca363f79d9943170eabfb9a8e71f6ad45b00
Henry Solomon <sehenrysolomon@outlook.com>
2020-10-08 01:41:06 -0700

    db settings for production added

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -148,5 +148,8 @@ LOGIN_URL = 'users:login'
 import django_heroku
 django_heroku.settings(locals())
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
